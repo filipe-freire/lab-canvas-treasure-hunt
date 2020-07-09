47df085baed23f4860b284cc079bd6376cc8661b
Filipe Freire <filipefreire@protonmail.com>
2020-07-09 20:43:46 +0100

    Complete all iterations except the Bonus

--- main.js
+++ main.js
@@ -5,13 +5,16 @@ const width = canvas.width;
 const height = canvas.height;
 
 const GRID_OUTLINE = '#000';
+// Square size
+const padding = 0;
+
+// generates a random number for the treasure
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 10) * 50;
+};
 
 // Iteration 1
 function drawGrid() {
-  // TODO: write the code of the function
-  // Square size
-  let padding = 0;
-
   // vertical lines
   for (let x = 0; x <= width; x += 50) {
     context.moveTo(x + padding, padding);
@@ -23,14 +26,123 @@ function drawGrid() {
     context.moveTo(padding, x + padding);
     context.lineTo(width + padding, x + padding);
   }
-  context.strokeStyle = 'black';
+
+  context.strokeStyle = GRID_OUTLINE;
   context.stroke();
+}
+
+// Iteration 2 - Character Class
+
+class Character {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  moveUp() {
+    this.row -= 50;
+  }
+  moveRight() {
+    this.col += 50;
+  }
+  moveDown() {
+    this.row += 50;
+  }
+  moveLeft() {
+    this.col -= 50;
+  }
+}
+
+const player = new Character(0, 0);
+
+// Iteration 3 - Drawing the Player
+
+function drawPlayer() {
+  // redraws grid to simulate movement
+  context.clearRect(0, 0, width, width);
+  drawGrid();
+
+  const playerSprite = new Image();
+  playerSprite.src = '/images/character-down.png';
+
+  // do this in order to prevent the image not loading on the page
+  playerSprite.addEventListener('load', () => {
+    // how to keep the ration in the images
+    const width = playerSprite.width;
+    const height = playerSprite.height;
+    const ratio = width / height;
+
+    context.drawImage(playerSprite, player.col, player.row, 50, 50 / ratio);
+  });
+}
+
+// Iteration 4
+
+class Treasure {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  setRandomPosition() {
+    this.col = generateRandomNumber();
+    this.row = generateRandomNumber();
+  }
+}
+
+const treasure = new Treasure(generateRandomNumber(), generateRandomNumber());
+
+function drawTreasure() {
+  // redraws grid to simulate movement
+  context.clearRect(0, 0, width, width);
+  drawGrid();
+
+  const treasureSprite = new Image();
+  treasureSprite.src = '/images/treasure.png';
+
+  // do this in order to prevent the image not loading on the page
+  treasureSprite.addEventListener('load', () => {
+    // how to keep the ration in the images
+    const width = treasureSprite.width;
+    const height = treasureSprite.height;
+    const ratio = width / height;
+
+    context.drawImage(treasureSprite, treasure.col, treasure.row, 50, 50 / ratio);
+  });
 }
 
 function drawEverything() {
   drawGrid();
-  // drawPlayer()
-  // drawTreasure()
+  drawPlayer();
+  drawTreasure();
 }
 
 drawEverything();
+
+// Iteration 5
+
+window.addEventListener('keydown', event => {
+  // Stop the default behavior (moving the screen to the left/up/right/down)
+  event.preventDefault();
+
+  // React based on the key pressed
+  switch (event.keyCode) {
+    case 37:
+      console.log('left');
+      player.moveLeft();
+      break;
+    case 38:
+      console.log('up');
+      player.moveUp();
+      break;
+    case 39:
+      console.log('right');
+      player.moveRight();
+      break;
+    case 40:
+      console.log('down');
+      player.moveDown();
+      break;
+  }
+
+  drawEverything();
+});
